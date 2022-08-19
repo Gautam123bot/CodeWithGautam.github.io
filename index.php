cdea6990ec636e1719fb818aa105a61429c47f19
Gautam <0101gautamkumar@gmail.com>
2022-08-19 14:52:36 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index page</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css.bootstrap.min.css">
+    <script src="js/bootstrap.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="app.js"></script>
+</head>
+
+<body>
+
+
+    <?php include 'header.php'; ?>
+
+    <section>
+        <div class="container">
+            <h1>Hi this is my personal site</h1>
+            <h2>Will be updated soon...</h2>
+        </div>
+    </section>
+
+
+
+
+    <?php include 'footer.php'; ?>
+
+
+</body>
+
+</html>
