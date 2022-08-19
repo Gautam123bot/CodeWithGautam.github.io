b3b2ce4a4be75e62be69f2bee280505f709e118d
Gautam <0101gautamkumar@gmail.com>
2022-08-20 00:57:41 +0530

    changed favicon x-icon

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index page</title>
+    <title>Gautam Kumar | Portfolio</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css.bootstrap.min.css">
     <script src="js/bootstrap.js"></script>
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="tabletmode.css">
     <link rel="stylesheet" href="mobilemode.css">
     <script src="app.js"></script>
+    <link rel="icon" type="image/x-icon" href="img/profile_photo.jpeg">
 </head>
 
 <body>
