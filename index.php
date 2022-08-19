82e10fdb4e03607e40a69a367a1d2e433a65dd73
Gautam <0101gautamkumar@gmail.com>
2022-08-19 22:48:36 +0530

    Update

--- index.php
+++ index.php
@@ -18,10 +18,18 @@
 
     <?php include 'header.php'; ?>
 
+
+
     <section>
-        <div class="container">
-            <h1>Hi this is my personal site</h1>
-            <h2>Will be updated soon...</h2>
+        <div class="container main-section">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <img src="img/profile_photo.jpeg" alt="" class="img-responsive">
+                    <p class="hey_there">Hey There,</p>
+                    <p class="iamgautam">I'm Gautam.</p>
+                    <p class="iamfullpara">I,m a full stack developer with a passion for everything front-end. Welcome to my corner of the internet. I,m glad you're here!</p>
+                </div>
+            </div>
         </div>
     </section>
 
