543d1f288472201327aa8e3e983dcd440391dd1b
Gautam <0101gautamkumar@gmail.com>
2022-09-01 23:54:30 +0530

    Another server commit

--- index.php
+++ index.php
@@ -35,8 +35,8 @@
                         to my corner of the internet. I,m glad you're here!</p>
                 </div>
             </div>
-            <button class="resumebtn"><a onclick="openTab(this)" href="#" name="PDF/codewithgautamresume.pdf">Download
-                    CV</a></button>
+            <button class="resumebtn"><a onclick="openTab(this)" href="#" name="PDF/codewithgautamresume.pdf">Check out
+                    my CV</a></button>
             <script type="text/javascript">
             function openTab(th) {
                 window.open(th.name, '_blank');
@@ -44,6 +44,21 @@
             </script>
         </div>
     </section>
+    <!-- <svg class="wave1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#00cba9" fill-opacity="1"
+            d="M0,128L60,144C120,160,240,192,360,202.7C480,213,600,203,720,181.3C840,160,960,128,1080,101.3C1200,75,1320,53,1380,42.7L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
+        </path>
+    </svg>
+    <svg class="wave2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#00cba9" fill-opacity="1"
+            d="M0,288L80,261.3C160,235,320,181,480,154.7C640,128,800,128,960,138.7C1120,149,1280,171,1360,181.3L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z">
+        </path>
+    </svg> -->
+
+    <!-- <div class="ocean">
+        <div class="wave"></div>
+        <div class="wave"></div>
+    </div> -->
 
 
     <?php include 'footer.php'; ?>
